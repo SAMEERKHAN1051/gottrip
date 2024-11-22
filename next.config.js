b0f5a70c79b1c4d72dedc793a097ecb2fc5e7396
Sameer Khan <113291729+SAMEERKHAN1051@users.noreply.github.com>
2024-11-22 22:44:54 +0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  assetPrefix: '/blog-static',
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
+
+module.exports = nextConfig;
